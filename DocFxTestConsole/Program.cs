1758e1748191ae173cb9d603bc093b454ed0713c
Xreality <JSHMorgan@outlook.com>
2023-04-24 11:16:15 +0100

    Initial commit

---
+++ DocFxTestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DocFxTest2;
+
+/// <summary>
+/// Class for the main program to sit.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Main function saying hello world.
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
